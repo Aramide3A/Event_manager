703fadbfcf0cc6f6d21622d7bc98286566f26696
Al-Ameen Ameen <alameenameen3101@gmail.com>
2025-05-21 17:04:49 +0100

    cors

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const express = require('express')
 const passport = require('passport')
 const authRouter = require('./src/routes/auth.routes')
 const eventRouter = require('./src/routes/register.routes')
+const cors = require('cors');
 require('dotenv').config()
 require('./src/utils/passportGoogle')
 require('./src/utils/passportJwt')
-const cors = require('cors');
 
-app.use(cors());
+
 const app = express()
+app.use(cors());
 app.use(passport.initialize())
 app.use(express.json())
 
